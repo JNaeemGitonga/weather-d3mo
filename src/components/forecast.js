afe623c1b00d4088fe010f1677e885ac06032abf
PROB8 <blackb8r@gmail.com>
2018-01-30 21:35:51 -0500

    added some text to wind ternary

--- src/components/forecast.js
+++ src/components/forecast.js
@@ -14,9 +14,12 @@ class Forecast extends Component {
                                 <li><img src={day.icon_url} alt={day.icon}/></li>
                                 <li>{day.date.weekday_short}</li>
                                 <li>{day.date.month}/{day.date.day}</li>
-                                <li>Humidity: {day.avehumidity}</li>
+                                
                                 <li>Conditions: {day.conditions}</li>
-                                <li>Hi: {day.high.fahrenheit}&#8457;/ {day.high.celsius}&#8451;</li>
+                                <li>Hi: {day.high.fahrenheit}&#8457; / {day.high.celsius}&#8451;</li>
+                                <li>Lo: {day.low.fahrenheit}&#8457; / {day.low.celsius}&#8451;</li>
+                                
+                                <li>Humidity: {day.avehumidity}</li><li>Wind: {day.maxwind.mph == -999 ? 'unavailable' : day.maxwind.mph + ' mph'}, {day.maxwind.dir == -999 ? 'unavailable' : day.maxwind.dir}</li>
                             </ul>
                         </div>
                     )

ac8e665b086d19d8e8487d2e568637d036c8d7b8
PROB8 <blackb8r@gmail.com>
2018-01-31 20:30:11 -0500

    update .gitignore to ignore random.js, update moment

--- src/components/date-time.js
+++ src/components/date-time.js
@@ -2,7 +2,6 @@ import React, {Component} from 'react';
 import {connect} from 'react-redux';
 import {updateTime} from '../actions';
 import moment from 'moment';
-import moment2 from 'moment-timezone';
 
 class DateTime extends Component {
 
@@ -17,22 +16,11 @@ class DateTime extends Component {
 
     startTime = () => {
         let today = new Date();
-        // let h = today.getHours();
-        // let m = today.getMinutes();
-        // let s = today.getSeconds();
-        // m = this.checkTime(m);
-        // s = this.checkTime(s);
         this.setState({date:moment(today).format('MMM Do YY')})
-        // console.log(moment(today.getTime()).format('LT'))
-        // this.props.dispatch(updateTime(`${h}:${m}:${s}`));
         this.props.dispatch(updateTime(moment(today.getTime()).format('LT')))
         setTimeout(this.startTime, 500);
     }
 
-    // checkTime = i => {
-    //     if (i < 10) {i = '0' + i}
-    //     return i
-    // }
 
     render() {
         const {date} = this.state;
